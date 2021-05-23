4a52fbb431d1a28973e30dbb28a673f59ff1af78
Vinícius Müller <vinigm.nho@gmail.com>
2021-05-23 12:16:55 -0300

    dwm: Change font

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Jetbrains Mono:size=9" };
+static const char dmenufont[]       = "JetbrainsMono:size=9";
 static const char col_white[]       = "#aaaaaa";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
