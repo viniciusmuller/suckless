447b242c0d929ecba65776f37f2c2aee40ce02a7
Victor Freire <victor@freire.dev.br>
2021-06-23 17:28:37 -0300

    added overlays

--- flake.nix
+++ flake.nix
@@ -5,40 +5,38 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.dmenu = (pkgs.dmenu.overrideAttrs (old: rec {
-          name = "dmenu-custom";
-          src = ./dmenu;
-        }));
-
-        packages.dwm = (pkgs.dwm.overrideAttrs (old: rec {
+  outputs = { self, nixpkgs, utils }: {
+    overlays = (final: prev: {
+      dwm = prev.dwm.overrideAttrs
+        (old: rec {
           name = "dwm-custom";
           src = ./dwm;
-        }));
+        });
 
-        packages.st = (pkgs.st.overrideAttrs (old: rec {
-          name = "st-custom";
-          buildInputs = old.buildInputs ++ [ pkgs.harfbuzz ];
-          src = ./st;
-        }));
-
-        packages.slstatus = (pkgs.slstatus.overrideAttrs (old: rec {
-          name = "slstatus-custom";
-          src = ./slstatus;
-        }));
-
-        packages.slock = (pkgs.slock.overrideAttrs (old: rec {
-          name = "slock-custom";
-          buildInputs = old.buildInputs ++ [
-            pkgs.xorg.libXinerama
-            pkgs.xorg.libXft
-          ];
-          src = ./slock;
-        }));
+      dmenu = prev.dmenu.overrideAttrs (old: rec {
+        name = "dmenu-custom";
+        src = ./dmenu;
       });
+
+      st = prev.st.overrideAttrs (old: rec {
+        name = "st-custom";
+        buildInputs = old.buildInputs ++ [ prev.harfbuzz ];
+        src = ./st;
+      });
+
+      slstatus = prev.slstatus.overrideAttrs (old: rec {
+        name = "slstatus-custom";
+        src = ./slstatus;
+      });
+
+      slock = prev.slock.overrideAttrs (old: rec {
+        name = "slock-custom";
+        buildInputs = old.buildInputs ++ [
+          prev.xorg.libXinerama
+          prev.xorg.libXft
+        ];
+        src = ./slock;
+      });
+    });
+  };
 }
